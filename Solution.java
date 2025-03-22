3f857f6def34cab0689fa92806d019f2ff159d6b
Soham217 <sohamchakraborty832@gmail.com>
2025-03-22 08:04:33 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    static FastReader sc = new FastReader();
+    static FastWriter out = new FastWriter();
+
+    private void solve() {
+        
+    }
+
+    public static void main(String[] soham) {
+        int testCases = sc.nextInt();
+        Solution obj = new Solution();
+        while (testCases-- > 0) {
+            obj.solve();
+        }
+        out.close();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine().trim();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) {
+            try {
+                bw.append(String.valueOf(object));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void println(Object object) {
+            print(object);
+            try {
+                bw.append("\n");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void close() {
+            try {
+                bw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
